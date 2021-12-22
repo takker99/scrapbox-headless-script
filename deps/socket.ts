c56a16c89cfd03548460f216b0c1e5c4bcf8e84b
takker99 <37929109+takker99@users.noreply.github.com>
2021-12-22 19:34:26 +0900

    :package: Pin the version of scrapbox-userscript-websocket

--- deps/socket.ts
+++ deps/socket.ts
@@ -8,8 +8,8 @@ export type {
   ProjectUpdatesStreamCommit,
   ProjectUpdatesStreamEvent,
   UpdateCommit,
-} from "https://pax.deno.dev/takker99/scrapbox-userscript-websocket";
+} from "https://pax.deno.dev/takker99/scrapbox-userscript-websocket@0.1.1";
 export {
   socketIO,
   wrap,
-} from "https://pax.deno.dev/takker99/scrapbox-userscript-websocket";
+} from "https://pax.deno.dev/takker99/scrapbox-userscript-websocket@0.1.1";

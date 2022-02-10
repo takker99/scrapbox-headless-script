9b54e17080dedd5dfaa262c6eb4ccf5782469931
takker99 <37929109+takker99@users.noreply.github.com>
2022-02-10 17:24:32 +0900

    :sparkles: Enable to pin and unpin pages

--- shortcuts.ts
+++ shortcuts.ts
@@ -1,7 +1,8 @@
-import { Change, socketIO, wrap } from "./deps/socket.ts";
+import { Change, Pin, socketIO, TitleCommit, wrap } from "./deps/socket.ts";
 import { getProjectId, getUserId } from "./id.ts";
 import { diffToChanges } from "./patch.ts";
 import { applyCommit } from "./applyCommit.ts";
+import { pinNumber } from "./pin.ts";
 import type { Line } from "./deps/scrapbox.ts";
 import { ensureEditablePage, pushCommit, pushWithRetry } from "./_fetch.ts";
 
@@ -134,6 +135,98 @@ export async function patch(
         }
       }
     }
+  } finally {
+    io.disconnect();
+  }
+}
+
+export interface PinOption {
+  /** ピン留め対象のページが存在しないときの振る舞いを変えるoption
+   *
+   * -`true`: タイトルのみのページを作成してピン留めする
+   * - `false`: ピン留めしない
+   *
+   * @default false
+   */
+  create: boolean;
+}
+/** 指定したページをピン留めする
+ *
+ * @param project ピン留めしたいページのproject
+ * @param title ピン留めしたいページのタイトル
+ */
+export async function pin(
+  project: string,
+  title: string,
+  option?: PinOption,
+): Promise<void> {
+  const [
+    { id: pageId, commitId: initialCommitId, persistent, pin },
+    projectId,
+    userId,
+  ] = await Promise.all([
+    ensureEditablePage(project, title),
+    getProjectId(project),
+    getUserId(),
+  ]);
+  let parentId = initialCommitId;
+
+  // 既にピン留めされている場合は何もしない
+  if (pin > 0 && !persistent && !(option?.create ?? false)) return;
+
+  const init = { projectId, pageId, userId, project, title };
+  const io = await socketIO();
+  const { request } = wrap(io);
+
+  // タイトルのみのページを作る
+  if (!persistent) {
+    parentId = await pushWithRetry(request, [{ title }], {
+      parentId,
+      ...init,
+    });
+  }
+
+  try {
+    parentId = await pushWithRetry(request, [{ pin: pinNumber() }], {
+      parentId,
+      ...init,
+    });
+  } finally {
+    io.disconnect();
+  }
+}
+/** 指定したページのピン留めを外す
+ *
+ * @param project ピン留めを外したいページのproject
+ * @param title ピン留めを外したいページのタイトル
+ */
+export async function unpin(
+  project: string,
+  title: string,
+): Promise<void> {
+  const [
+    { id: pageId, commitId: initialCommitId, persistent, pin },
+    projectId,
+    userId,
+  ] = await Promise.all([
+    ensureEditablePage(project, title),
+    getProjectId(project),
+    getUserId(),
+  ]);
+  let parentId = initialCommitId;
+
+  // 既にピンが外れているか、そもそも存在しないページの場合は何もしない
+  if (pin == 0 || !persistent) return;
+
+  const init = { projectId, pageId, userId, project, title };
+  const io = await socketIO();
+  const { request } = wrap(io);
+
+  try {
+    parentId = await pushWithRetry(request, [{ pin: 0 }], {
+      parentId,
+      ...init,
+    });
   } finally {
     io.disconnect();
   }
